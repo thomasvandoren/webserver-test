5571da28bb4bb5ad318fe6480bca689fad8d4212
Thomas Van Doren <thomas.vandoren@gmail.com>
2015-05-26 19:57:59 -0700

    Add python with flask implementation.

---
+++ python2/server.py
@@ -0,0 +1,48 @@
+"""Python web server.
+
+Requires Flask. Install with:
+
+    pip install Flask
+
+or:
+
+    pip install -r requirements.txt
+
+"""
+
+import datetime
+import flask
+import json
+import uuid
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_it():
+    data = {
+        "utc_datetime": utcnow().isoformat(),
+        "uuid": str(uuid.uuid4()),
+    }
+    return flask.jsonify(data)
+
+
+@app.route('/uuid/<the_uuid>', methods=['POST'])
+def post_it(the_uuid):
+    try:
+        the_uuid = uuid.UUID(the_uuid)
+    except ValueError as ex:
+        return 400, ex.message  # Yep, this isn't the best thing to return...
+    data = {
+        "utc_datetime": utcnow().isoformat(),
+        "uuid": str(the_uuid),
+    }
+    return flask.jsonify(data)
+
+
+def utcnow():
+    return datetime.datetime.utcnow().replace(microsecond=0)
+
+
+if __name__ == '__main__':
+    app.run()
